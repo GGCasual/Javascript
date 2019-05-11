2d536e98d00b9d17e187e80764d7887813ac1924
GGCasual <gc.favorito95@gmail.com>
2019-05-10 23:17:23 -0500

    week 4

--- 5-8_Homework.js
+++ 5-8_Homework.js
@@ -154,12 +154,18 @@
 // 3 times
 // Given this function:
 
-// function call3Times(fun) { fun(); fun(); fun(); }
-// Use the call3Times function to print "Hello, world!" 3 times.
-function func10(){
 
-}
-return func10();
+// Use the call3Times function to print "Hello, world!" 3 times.
+// function fun(){
+//     console.log("Hello, world!");
+// }
+
+// function call3Times(){
+//     fun(); 
+//     fun(); 
+//     fun(); 
+// }
+// return call3Times();
 
 
 
